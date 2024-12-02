c5071aa4306ca6ad9a1645ec925396fc08538ece
Discente <Discente@etec224.edu.br>
2024-12-02 16:44:39 -0300

    first commit

--- main.js
+++ main.js
@@ -36,26 +36,3 @@ ScrollReveal().reveal(".section-4", {
   delay: 2500,
 });
 
-
-// Seleciona todos os botões da página
-const buttons = document.querySelectorAll('button');
-
-// Adiciona um evento de clique para redirecionar todos os botões
-buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        event.preventDefault(); // Impede o comportamento padrão
-        window.location.href = 'error.html'; // Redireciona para a página de erro
-    });
-});
-
-
-// Seleciona todos os links e botões
-const interactiveElements = document.querySelectorAll('button, a, .item-link-viaje-pelo, .item-link-ajuda');
-
-// Adiciona um evento de clique para redirecionar
-interactiveElements.forEach(element => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault(); // Impede o comportamento padrão
-        window.location.href = 'error.html'; // Redireciona para a página de erro
-    });
-});
